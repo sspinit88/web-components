1fe08749cfa3e43fc0da7bbeabc0c9e251f40ffe
Sergey Pashnin <sspinit88@gmail.com>
2020-02-09 11:29:10 +0300

    added fetch

--- StencilJS/stencil-complex/src/components/stock-price/stock-price.tsx
+++ StencilJS/stencil-complex/src/components/stock-price/stock-price.tsx
@@ -1,4 +1,4 @@
-import { Component, h } from '@stencil/core';
+import { Component, h, State } from '@stencil/core';
 
 @Component({
   tag: 'u-stock-price',
@@ -7,19 +7,34 @@ import { Component, h } from '@stencil/core';
 })
 export class StockPrice {
 
+  @State() fetchPrice: number;
+
   onFetchStockPrice(e: Event): void {
     e.preventDefault();
+
+    fetch('https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=MSFT&apikey=demo')
+      .then(res => {
+        return res.json();
+      })
+      .then(parseRes => {
+        this.fetchPrice = +parseRes['Global Quote']['05. price'];
+      })
+      .catch(
+        err => {
+          console.log('File: stock-price.tsx, Line - 17, err:', err);
+        }
+      );
   }
 
   render() {
     return [
-      <form onSubmit={this.onFetchStockPrice}
+      <form onSubmit={this.onFetchStockPrice.bind(this)}
             class="form">
         <input class="stock-symbol"/>
         <button type="submit">Fetch</button>
       </form>,
       <div>
-        <p>Price: {0}</p>
+        <p>Price: ${!!this.fetchPrice ? this.fetchPrice : 0}</p>
       </div>
     ];
   }
