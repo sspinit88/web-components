9fb7f3609c1ab54969a1597d725d312f94cb0cd7
Sergey Pashnin <sspinit88@gmail.com>
2020-02-14 13:43:57 +0300

    закончил работу над первым компонентом

--- StencilJS/stencil-complex/src/components/stock-price/stock-price.tsx
+++ StencilJS/stencil-complex/src/components/stock-price/stock-price.tsx
@@ -1,4 +1,4 @@
-import { Component, h, State, Element, Prop } from '@stencil/core';
+import { Component, h, State, Element, Prop, Watch } from '@stencil/core';
 
 import { AV_API_KEY } from '../global/global'
 
@@ -10,6 +10,7 @@ import { AV_API_KEY } from '../global/global'
 export class StockPrice {
 
   stockInput: HTMLInputElement;
+  initialStockSymbol: string;
 
   //// получаем доступк к элементу
   @Element() el: HTMLElement;
@@ -20,22 +21,83 @@ export class StockPrice {
   @State() error: string;
 
   /*
-  *  устанавливаем значение атрибута
+  *  устанавливаем значение атрибута,
+  * получаем данные извне
   * */
-  @Prop() stockSymbol: string;
+  @Prop({
+    mutable: true,
+    reflect: true,
+  }) stockSymbol: string;
 
-  componentDidLoad() {
+  /*
+  * Когда пользователь обновляет свойство,
+  * Watch запускает любой метод, к которому он присоединен,
+  * и передает этому методу новое значение свойства вместе со старым значением.
+  * Watch - полезно для проверки реквизита или обработки побочных эффектов.
+  * Watch декоратор не срабатывает при начальной загрузке компонента.
+  * */
+  @Watch('stockSymbol')
+  stockSymbolChanged(newValue: string, oldValue: string): void {
+    if (newValue !== oldValue) {
+      this.stockUserInput = this.stockSymbol;
+      this.fetchStockPrice(newValue);
+    }
+  }
+
+  componentWillLoad(): void {
+    console.log('componentWillLoad()');
+    console.log('componentWillLoad(), this.stockSymbol:', this.stockSymbol)
+  }
+
+  componentDidLoad(): void {
     /*
     * componentDidMount() вызывается сразу после монтирования
     * (то есть, вставки компонента в DOM).
     * В этом методе должны происходить действия, которые требуют наличия DOM-узлов.
     * Это хорошее место для создания сетевых запросов.
     * */
+    console.log('componentDidLoad()');
     if (this.stockSymbol) {
+      /*
+      * устанавливаем стартовое значение в инпут
+      * */
+      this.initialStockSymbol = this.stockSymbol;
+      this.stockUserInput = this.stockSymbol;
+      this.stockUserInputValid = true;
+
       this.fetchStockPrice(this.stockSymbol);
     }
   }
 
+  componentWillUpdate(): void {
+    /*
+    * срабатывает перед повторным рендеренгом,
+    * из-за изменения @Prop() или @State()
+    * */
+    console.log('componentWillUpdate()');
+  }
+
+  componentDidUpdate(): void {
+    console.log('componentDidUpdate()');
+    /*
+    * componentDidUpdate() вызывается сразу после обновления.
+    * Не вызывается при первом рендере.
+    * Метод позволяет работать с DOM при обновлении компонента.
+    * Также он подходит для выполнения таких сетевых запросов,
+    * которые выполняются на основании результата сравнения текущих пропсов с предыдущими.
+    *  Если пропсы не изменились, новый запрос может и не требоваться.
+    * */
+
+    // if (this.stockSymbol !== this.initialStockSymbol) {
+    //   this.initialStockSymbol = this.stockSymbol;
+    //   this.fetchStockPrice(this.stockSymbol);
+    //   this.stockUserInput = this.stockSymbol;
+    // }
+  }
+
+  componentDidUnload(): void {
+    console.log('componentDidUnload()');
+  }
 
   onUserInput(e: Event): void {
     this.stockUserInput = (e.target as HTMLInputElement).value;
@@ -55,8 +117,8 @@ export class StockPrice {
     //   ) as HTMLInputElement).value;
 
     //// второй вариант получения значения из инпута
-    const stockSymbol: string = this.stockInput.value;
-    this.fetchStockPrice(stockSymbol)
+    this.stockSymbol = this.stockInput.value;
+    // this.fetchStockPrice(stockSymbol)
 
   }
 
